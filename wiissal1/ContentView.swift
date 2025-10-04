051e02b4d7859c7e3da87d7af6dcbd9cef82a255
lujain <janealrugabi@gmail.com>
2025-10-04 16:01:08 +0300

    Initial Commit

---
+++ wiissal1/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  wiissal1
+//
+//  Created by Lujain Alrugabi on 04/10/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ wiissal1/wiissal1App.swift
@@ -0,0 +1,17 @@
+//
+//  wiissal1App.swift
+//  wiissal1
+//
+//  Created by Lujain Alrugabi on 04/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct wiissal1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
